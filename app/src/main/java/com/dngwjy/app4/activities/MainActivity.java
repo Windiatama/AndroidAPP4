e9944b2c40b6974f793a6e379d2a54f27382676e
Windiatama <40048695+Windiatama@users.noreply.github.com>
2018-11-07 12:30:56 +0700

    Android

---
+++ app/src/main/java/com/dngwjy/app4/activities/MainActivity.java
@@ -0,0 +1,22 @@
+package com.dngwjy.app4.activities;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.support.v7.widget.RecyclerView;
+
+import com.dngwjy.app4.R;
+import com.dngwjy.app4.data.models.MasjidModel;
+import com.dngwjy.app4.views.MainView;
+
+import java.util.List;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+
+}
